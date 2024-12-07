f638568ba5fb4cb3b97bc2271c9fcbfb5b90267f
Juan <jlalvac@uni.pe>
2024-12-07 12:46:17 -0500

    feat: :sparkles: add HTML of Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import viteLogo from "/vite.svg";
+
+function Home() {
+  return (
+    <main>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <h1>AuditFlow</h1>
+      <p>Herramienta para auditorias</p>
+    </main>
+  );
+}
+
+export default Home;
