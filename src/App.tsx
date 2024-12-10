297ea3fd97fa258a2fdbf23660e2347f5d7224f5
Juan <jlalvac@uni.pe>
2024-12-10 10:07:22 -0500

    chore: :sparkles: Add router to pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Home from "./pages/Home";
 
 import SignIn from "./pages/sign-in/SignIn";
 import SignUp from "./pages/sign-up/SignUp";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import NewAudit from "./pages/NewAudit";
+import RiskMatrix from "./pages/RiskMatrix";
+import Walkthrough from "./pages/Walkthrough";
 
 function App() {
   return (
@@ -13,6 +18,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/new-audit" element={<NewAudit />} />
+        <Route path="/risk-matrix/:id" element={<RiskMatrix />} />
+        <Route path="/walkthrough/:id" element={<Walkthrough />} />
       </Routes>
     </>
   );
